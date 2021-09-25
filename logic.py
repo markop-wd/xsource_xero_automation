b3dd98e5afae91508ef6eb10c8b490b0fbc917fa
Marko P <markop@mailbox.org>
2021-09-25 10:26:18 +0200

    Refactoring of logic.py

--- logic.py
+++ logic.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait as WdWait
 from selenium.webdriver import Chrome
 
-from helper_funcs import element_clicker, element_waiter, login_element_waiter, LoginException, OrgFinderException
+from helper_funcs import element_clicker, element_waiter, login_element_waiter, LoginException
 from urls import INVOICE_STATUS, PAGE_SEARCH
 
 logger = logging.getLogger(__name__)
@@ -25,7 +25,6 @@ def log_in(driver: Chrome):
     :return:
     """
     driver.get('https://login.xero.com/')
-    # TODO - Extract the three WdWaits with try except in a pattern
     input("Please press enter once you login")
     try:
         login_element_waiter(driver)
@@ -34,46 +33,45 @@ def log_in(driver: Chrome):
         driver.quit()
 
 
-# TODO Refactor and recheck
-def org_switch(driver: Chrome, org_name: str):
+def org_switch(driver: Chrome, org_name: str) -> str:
     """
     Changing organizations within Xero
     :param driver:
-    :param org_name:
-    :return:
+    :param org_name: Organization name to search for
+    :return current_org: An empty string if no organization is found, otherwise the org name
     """
     current_org = ''
     org_button_text = WdWait(driver, 10).until(
         ec.presence_of_element_located((By.CLASS_NAME, 'xrh-appbutton--text'))).text
+    # If the name of the current organization does not match the element that identifies the org in xero move on
     if org_name.lower() != org_button_text.lower:
         element_clicker(driver, css_selector='.xrh-button.xrh-appbutton')
         element_clicker(driver, css_selector='.xrh-button.xrh-verticalmenuitem--body')
         try:
+            # Try using the input element to search for an org
             WdWait(driver, 6).until(ec.presence_of_element_located(
                 (By.CLASS_NAME, 'xrh-orgsearch--input'))).send_keys(org_name.lower())
         except exceptions.TimeoutException:
+            # If there is no input element for search, try searching through the available org list
             for link_item in driver.find_elements_by_class_name('xrh-menuitem-orgpractice'):
-                if link_item.text == org_name:
+                if link_item.text.lower() == org_name.lower():
                     element_clicker(driver, web_element=link_item)
         else:
-            try:
-                WdWait(driver, 5).until(
-                    ec.presence_of_element_located((By.CLASS_NAME, 'xrh-menuitem-orgpractice')))
-            except exceptions.TimeoutException as exc:
-                raise OrgFinderException(f'Cannot find {org_name}') from exc
-            else:
-                sleep(2)
-                orgs = driver.find_elements_by_class_name('xrh-menuitem-orgpractice')
-                org_names = [orggg.text for orggg in
-                             driver.find_elements_by_class_name('xrh-menuitem-orgpractice')]
-            for element in orgs:
-                if element.text.lower() == org_name.lower():
-                    element_clicker(driver, css_selector='.xrh-menuitem-orgpractice')
+            # find all the elements that are returned after typing the org name
+            WdWait(driver, 5).until(
+                ec.presence_of_element_located((By.CLASS_NAME, 'xrh-menuitem-orgpractice')))
+            sleep(2)
+            available_orgs = driver.find_elements_by_class_name('xrh-menuitem-orgpractice')
+            # Store org names in case we don't find a match and want to provide user with org names
+            org_names = [orggg.text for orggg in available_orgs]
+
+            for individual_org in available_orgs:
+                if individual_org.text.lower() == org_name.lower():
+                    element_clicker(driver, web_element=individual_org)
                     sleep(5)
-                    current_org = WdWait(driver, 25).until(ec.presence_of_element_located(
-                        (By.CLASS_NAME, 'xrh-appbutton--text'))).text
-                    if current_org.lower() != org_name.lower():
-                        raise OrgFinderException(f'Cannot find {org_name}')
+                    WdWait(driver, 25).until(ec.presence_of_element_located(
+                        (By.CLASS_NAME, 'xrh-appbutton--text')))
+                    current_org = org_name
                     break
             else:
 
@@ -81,21 +79,7 @@ def org_switch(driver: Chrome, org_name: str):
                                       org_name.lower() in _org_name.lower()]
                 if matching_org_names:
                     print('No exact match with that organisation name - did you mean?')
-                    for org in matching_org_names:
-                        print(f"  {org}")
-
-                org_name = input('Please re-input the exact organisation name: ').lower()
-                for organis in driver.find_elements_by_class_name('xrh-menuitem-orgpractice'):
-                    if org_name.lower() == organis.text.lower():
-                        element_clicker(driver, web_element=organis)
-                        sleep(5)
-                        current_org = WdWait(driver, 25).until(ec.presence_of_element_located(
-                            (By.CLASS_NAME, 'xrh-appbutton--text'))).text
-                        if current_org.lower() != org_name.lower():
-                            raise OrgFinderException(f'Cannot find {org_name}')
-                        break
-                else:
-                    raise OrgFinderException(f'Cannot find {org_name}')
+                    print(matching_org_names)
 
     else:
         current_org = org_name
@@ -103,7 +87,6 @@ def org_switch(driver: Chrome, org_name: str):
     return current_org
 
 
-# TODO - Refactor and recheck
 def invoice_pay(driver: Chrome, total_list: list) -> None:
     """
     Takes all the invoice urls and completes the action/calculations needed to pay them
@@ -124,6 +107,7 @@ def invoice_pay(driver: Chrome, total_list: list) -> None:
                 allocate_credit_btn = driver.find_element_by_css_selector(
                     'dd > ul > li > a[href*="/Credits/Allocate"]')
             except exceptions.NoSuchElementException:
+                # The allocate credit button was not found meaning there is nothing to allocate, skip
                 continue
             else:
                 allocate_url = allocate_credit_btn.get_attribute('href')
@@ -134,68 +118,84 @@ def invoice_pay(driver: Chrome, total_list: list) -> None:
                     logger.error("Could not load: %s", allocate_url)
                     continue
 
-                remaining_credit_text = driver.find_element_by_id('BalanceDue').get_attribute(
-                    'innerText')
-                remaining_credit_text = re.sub(',', '', remaining_credit_text)
-                remaining_credit = round(float(remaining_credit_text), 2)
-                bill_row_array = driver.find_element_by_id(
-                    'creditLineItems').find_elements_by_tag_name(
-                    'tr')
-                for bill_row in bill_row_array:
+                allocation_input(driver)
 
-                    row_due_amount = round(
-                        float(bill_row.find_element_by_css_selector('td > input').get_attribute('value')), 2)
+                allocation_finalize(driver, href, more_to_allocate)
 
-                    if remaining_credit > 0:
-                        if row_due_amount - remaining_credit >= 0:
-                            bill_row.find_element_by_css_selector(
-                                'td > div > span > input').send_keys(
-                                str(remaining_credit))
-                            remaining_credit -= remaining_credit
 
-                        elif row_due_amount - remaining_credit < 0:
-                            bill_row.find_element_by_css_selector(
-                                'td > div > span > input').send_keys(str(row_due_amount))
-                            remaining_credit -= row_due_amount
+def allocation_finalize(driver, href, more_to_allocate):
+    """
+    Wait for proper elements to load up, and log the results into the console and the csv
+    """
+    # allocate (finalize) button
+    element_clicker(driver, css_selector='.large.green')
+    try:
+        WdWait(driver, 10).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'document.invoice')))
+    except exceptions.TimeoutException:
+        logger.error('Unable to re-load the invoice page for confirmation.')
+        csv_append(href, more_to_allocate=None, error=True)
 
-                    elif remaining_credit < 0:
-                        # print("Calculation Error, didn't fill in", href)
-                        logger.error('Went into a negative value when filling %s', driver.current_url)
-                        break
+    else:
+        try:
+            driver.find_element_by_css_selector(
+                'dd > ul > li > a[href*="/Credits/Allocate"]')
+        except exceptions.NoSuchElementException:
+            pass
+        else:
+            more_to_allocate = True
 
-                # allocate (finalize) button
-                element_clicker(driver, css_selector='.large.green')
+        csv_append(href, more_to_allocate)
 
-                try:
-                    WdWait(driver, 10).until(
-                        ec.presence_of_element_located((By.CLASS_NAME, 'document.invoice')))
-                except exceptions.TimeoutException:
-                    logger.error('Unable to re-load the invoice page for confirmation.')
+        logger.info('Allocated %s', driver.current_url)
 
-                    with open('compare_csv.csv', mode='a', newline='', encoding='UTF-8') as checker:
-                        fieldnames = ['Link', 'More To Allocate', 'Error']
-                        checker = csv.DictWriter(checker, fieldnames=fieldnames)
 
-                        checker.writerow(
-                            {"Link": href, "More To Allocate": None, "Error": True})
+def csv_append(href, more_to_allocate, error=None):
+    """
+    Simple function that writes to a .csv
+    """
+    with open('compare_csv.csv', mode='a', newline='', encoding='UTF-8') as checker:
+        fieldnames = ['Link', 'More To Allocate', 'Error']
+        checker = csv.DictWriter(checker, fieldnames=fieldnames)
 
-                else:
-                    try:
-                        driver.find_element_by_css_selector(
-                            'dd > ul > li > a[href*="/Credits/Allocate"]')
-                    except exceptions.NoSuchElementException:
-                        pass
-                    else:
-                        more_to_allocate = True
+        checker.writerow(
+            {"Link": href, "More To Allocate": more_to_allocate, "Error": error})
 
-                    with open('compare_csv.csv', mode='a', newline='', encoding='UTF-8') as checker:
-                        fieldnames = ['Link', 'More To Allocate', 'Error']
-                        checker = csv.DictWriter(checker, fieldnames=fieldnames)
 
-                        checker.writerow(
-                            {"Link": href, "More To Allocate": more_to_allocate, "Error": None})
+def allocation_input(driver: Chrome):
+    """
+    The function that is used to handle the input elements and it also houses the logic for allocation calculations
+    """
+    # Remaining credit is how much we have available for allocation
+    remaining_credit_text = driver.find_element_by_id('BalanceDue').get_attribute('innerText')
+    # Remove commas that are used to split above three digit figures e.g. 1,342
+    remaining_credit_text = re.sub(',', '', remaining_credit_text)
+    remaining_credit = round(float(remaining_credit_text), 2)
+    # Find all the rows with the input els where the funds need to be allocated
+    bill_row_array = driver.find_element_by_id(
+        'creditLineItems').find_elements_by_tag_name(
+        'tr')
+    for bill_row in bill_row_array:
 
-                    logger.info('Allocated %s', driver.current_url)
+        row_due_amount = round(
+            float(bill_row.find_element_by_css_selector('td > input').get_attribute('value')), 2)
+
+        if remaining_credit > 0:
+            if row_due_amount - remaining_credit >= 0:
+                bill_row.find_element_by_css_selector(
+                    'td > div > span > input').send_keys(
+                    str(remaining_credit))
+                remaining_credit -= remaining_credit
+
+            elif row_due_amount - remaining_credit < 0:
+                bill_row.find_element_by_css_selector(
+                    'td > div > span > input').send_keys(str(row_due_amount))
+                remaining_credit -= row_due_amount
+
+        elif remaining_credit < 0:
+            # print("Calculation Error, didn't fill in", href)
+            logger.error('Went into a negative value when filling %s', driver.current_url)
+            break
 
 
 def href_extraction(driver: Chrome):
